07d333ddf24a63288b096ffc03e61f46d1eb53c8
GamesTrap <GamesTrap@web.de>
2025-11-11 00:23:31 +0100

    Updated Copyright

--- .modules/collate_modules/_manifest.lua
+++ .modules/collate_modules/_manifest.lua
@@ -3,7 +3,8 @@
 -- Purpose:     Define the collate_modules action.
 -- Author:      Jan "GamesTrap" Schürkamp
 -- Created:     2023/12/10
--- Copyright:   (c) 2023 Jan "GamesTrap" Schürkamp
+-- Updated:     2025/11/11
+-- Copyright:   (c) 2023-2025 Jan "GamesTrap" Schürkamp
 --
 
 return {
--- _preload.lua
+++ _preload.lua
@@ -2,8 +2,10 @@
 -- Name:        premake-ninja/_preload.lua
 -- Purpose:     Define the ninja action.
 -- Author:      Dmitry Ivanov
+-- Modified by: Jan "GamesTrap" Schürkamp
 -- Created:     2015/07/04
--- Copyright:   (c) 2015 Dmitry Ivanov, (c) 2023 Jan "GamesTrap" Schürkamp
+-- Updated:     2025/11/11
+-- Copyright:   (c) 2015 Dmitry Ivanov, (c) 2023-2025 Jan "GamesTrap" Schürkamp
 --
 
 local p = premake
